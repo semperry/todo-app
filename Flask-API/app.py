5c0a37f141e1f83a569a966cc0ea0d3f1a95508c
semperry <ryan.curtis88@gmail.com>
2020-07-17 10:36:14 -0600

    finalize routes. tested through postman

--- Flask-API/app.py
+++ Flask-API/app.py
@@ -64,8 +64,26 @@ def add_todo():
 	todo = Todo.query.get(new_todo.id)
 	return todo_schema.jsonify(todo)
 
+# PATCH
+@app.route("/todo/<id>", methods=["PATCH"])
+def update_todo(id):
+	todo = Todo.query.get(id)
+	new_done = request.json["done"]
+
+	todo.done = new_done
+	db.session.commit()
+
+	return todo_schema.jsonify(todo)
+
 # DELETE
-# PUT / PATCH
+@app.route("/todo/<id>", methods=["DELETE"])
+def delete_todo(id):
+	record = Todo.query.get(id)
+
+	db.session.delete(record)
+	db.session.commit()
+
+	return jsonify({"message": "DELETED THAT ISH!"})
 
 if __name__ == "__main__":
 	app.run(debug=True)
