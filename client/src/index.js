f49b54fe88490efa4627b5dc4227e63e73fe00e4
semperry <ryan.curtis88@gmail.com>
2020-07-20 10:10:52 -0600

    installs axios for http requests. requests data from prod api. renders todo title on screen

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from "axios"
 
 import './styles.css';
 
@@ -7,13 +8,38 @@ class App extends Component {
 	constructor() {
 		super()
 
-		this.state = {}
+		this.state = {
+			todos: []
+		}
+	}
+
+	renderTodos = () => {
+		return this.state.todos.map(todo => {
+			return (
+				<div key={todo.id}>
+					{todo.title}
+				</div>
+			)
+		})
+	}
+
+	componentDidMount() {
+		axios
+			.get('https://rec-flask-api.herokuapp.com/todos')
+			.then(response => this.setState({
+				todos: response.data
+			})
+			)
+			.catch(err => console.warn("Fetch Todos Err: ", err))
 	}
 
 	render() {
 		return (
 			<div>
 				<h1>Todo App</h1>
+				<div />
+
+				{this.renderTodos()}
 			</div>
 		)
 	}
